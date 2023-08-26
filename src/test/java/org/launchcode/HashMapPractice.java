5aff3e3596c772c74f2987e65dc836c55fba5d2c
Jake Willman <jakenwillman@gmail.com>
2023-08-26 15:29:33 -0500

    Finished Control Flow and Collections Exercise

--- src/test/java/org/launchcode/HashMapPractice.java
+++ src/test/java/org/launchcode/HashMapPractice.java
@@ -1,4 +1,37 @@
 package org.launchcode;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Map;
 
 public class HashMapPractice {
+    public static void main(String[] args) {
+        HashMap<Integer, String> classRoster = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        String newStudent;
+
+        System.out.println("Enter your students (or ENTER to finish):");
+
+        do {
+            System.out.print("Student: ");
+            newStudent = input.nextLine();
+
+            if (!newStudent.isEmpty()) {
+                System.out.print("ID: ");
+                Integer newID = input.nextInt();
+                classRoster.put(newID, newStudent);
+
+                input.nextLine();
+            }
+        } while (!newStudent.isEmpty());
+
+        input.close();
+
+        System.out.println("\nClass roster:");
+
+        for (Map.Entry<Integer, String> student : classRoster.entrySet()) {
+            System.out.println(student.getValue() + "'s ID: " + student.getKey());
+        }
+
+        System.out.println("Number of students in roster: " + classRoster.size());
+    }
 }
